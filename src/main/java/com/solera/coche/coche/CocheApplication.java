cc81323b32ccc9190846b01fbc4e5378824b36f1
Matiarriete <47977071+Matiarriete@users.noreply.github.com>
2022-11-23 00:54:25 +0100

    Modificaciones

--- src/main/java/com/solera/coche/coche/CocheApplication.java
+++ src/main/java/com/solera/coche/coche/CocheApplication.java
@@ -21,12 +21,21 @@ public class CocheApplication {
 		int[][] matriz = Helper.fillMatriz(8);
 		matriz = Helper.fillObstacles(matriz);
 		
-		int[][] matrizObstaculos = Helper.fillMatriz(8);
-		
 		for(int i = 0; i < 100; i++) {
 			System.out.println("nuevo movimiento");
 			String move = sc.nextLine();
-			Helper.move( move);
+			Helper.move(move);
+			if(matriz[Helper.x][Helper.y] == 2){
+				System.out.println("El coche cayo por un barranco y haz perdido todas tus vidas. GAME OVER");
+				return;
+			}
+			if(matriz[Helper.x][Helper.y] == 1){
+				System.out.println("El coche entro en barro por lo tanto pierdes una vida");
+				Helper.vidas--;
+				System.out.println("Debera moverse de sitio para salir de aqui");
+				i--;
+				matriz[Helper.x][Helper.y] = 2;
+			}
 			int[] cercanos = Helper.mostrarCercanos(Helper.x, Helper.y, matriz);
 			for (int j = 0; j < cercanos.length; j++) {
 				System.out.print(cercanos[j]);
